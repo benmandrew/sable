dcc3ca7c38445ed93736a5d2d9c23b2a5663232d
benmandrew <benmandrew@gmail.com>
2024-06-06 15:41:59 +0200

    Multithreading working with one thread!! Massive news!!

--- src/grid.rs
+++ src/grid.rs
@@ -1,8 +1,6 @@
+use crossbeam::scope;
 use rand::{rngs::ThreadRng, Rng};
-use std::{
-    thread::{self, Thread},
-    time::Duration,
-};
+use std::{sync::Arc, thread, time};
 
 pub struct Config {
     width: usize,
@@ -10,22 +8,22 @@ pub struct Config {
     size: usize,
     n_threads: usize,
     ribbon_len: usize,
-    rng: ThreadRng,
+    // rng: ThreadRng,
 }
 
 impl Config {
     fn new(width: usize, height: usize) -> Config {
         let size = width * height;
-        let n_threads = 4;
-        let ribbon_len = width * height / (n_threads * 2);
-        let rng = rand::thread_rng();
+        let n_threads = 1;
+        let ribbon_len = size / n_threads;
+        // let rng = rand::thread_rng();
         Config {
             width,
             height,
             size,
             n_threads,
             ribbon_len,
-            rng,
+            // rng,
         }
     }
 
@@ -42,66 +40,26 @@ impl Config {
     fn coords_to_index(&self, x: usize, y: usize) -> usize {
         y * self.width + x
     }
-
-    /// split array `arr` into `len(arr)/step` chunks, returning the first `slice_len` elements of each chunk
-    fn split<'a>(
-        &self,
-        arr: &'a Vec<u8>,
-        start: usize,
-        step: usize,
-        slice_len: usize,
-    ) -> Vec<&'a [u8]> {
-        assert!(slice_len <= step);
-        assert_eq!(self.size % slice_len, start);
-        let mut chunks = Vec::new();
-        let mut arr: &[u8] = &arr[start..];
-        // let end_i = (self.cfg.size / slice_len) - start;
-        for _i in 0..self.n_threads {
-            let (fst, snd) = arr.split_at(slice_len);
-            chunks.push(fst);
-            (_, arr) = snd.split_at(step - slice_len);
-        }
-        chunks
-    }
-
-    fn split_mut<'a>(
-        &self,
-        arr: &'a mut Vec<u8>,
-        start: usize,
-        step: usize,
-        slice_len: usize,
-    ) -> Vec<&'a mut [u8]> {
-        assert!(slice_len <= step);
-        assert_eq!(self.size % slice_len, start);
-        let mut v = Vec::new();
-        let mut a: &mut [u8] = &mut arr[start..];
-        // let end_i = (self.cfg.size / slice_len) - start;
-        for i in 0..self.n_threads {
-            let (fst, snd) = a.split_at_mut(i * step + slice_len);
-            v.push(fst);
-            (_, a) = snd.split_at_mut(step - slice_len)
-        }
-        assert_eq!(v.len(), self.n_threads);
-        v
-    }
 }
 
 fn next_pixel(
-    cfg: &mut Config,
-    source: &Vec<u8>,
-    target: &mut Vec<u8>,
-    index: usize,
+    cfg: &Config,
+    source: &[u8],
+    target: &mut [u8],
+    ribbon_i: usize,
+    real_i: usize,
 ) {
-    let (x, y) = cfg.index_to_coords(index);
+    let (x, y) = cfg.index_to_coords(ribbon_i);
+    let (_, real_y) = cfg.index_to_coords(real_i);
     let mut moved = false;
-    if y < cfg.height - 1 && source[index] != 0 {
-        let below = index + cfg.width;
+    if real_y < cfg.height - 1 && source[ribbon_i] != 0 {
+        let below = ribbon_i + cfg.width;
         if source[below] == 0 {
-            target[below] = source[index];
+            target[below - cfg.width] = source[ribbon_i];
             moved = true;
         }
-        let lateral_modifier = cfg.rng.gen_range(0..2) as isize * 2 - 1;
-        // let lateral_modifier = 1;
+        // let lateral_modifier = cfg.rng.gen_range(0..2) as isize * 2 - 1;
+        let lateral_modifier = 1;
         if x as isize + lateral_modifier >= 0
             && x as isize + lateral_modifier < cfg.width as isize
             && !moved
@@ -111,7 +69,7 @@ fn next_pixel(
                 y + 1,
             );
             if source[below_lateral] == 0 {
-                target[below_lateral] = source[index];
+                target[below_lateral - cfg.width] = source[ribbon_i];
                 moved = true;
             }
         }
@@ -124,16 +82,17 @@ fn next_pixel(
                 y + 1,
             );
             if source[below_lateral] == 0 {
-                target[below_lateral] = source[index];
+                target[below_lateral - cfg.width] = source[ribbon_i];
                 moved = true;
             }
         }
     }
-    if moved {
-        target[index] = 0;
-    } else if !moved && source[index] != 0 {
-        // } else {
-        target[index] = source[index];
+    if ribbon_i >= cfg.width {
+        if moved {
+            target[ribbon_i - cfg.width] = 0;
+        } else if !moved && source[ribbon_i] != 0 {
+            target[ribbon_i - cfg.width] = source[ribbon_i];
+        }
     }
 }
 
@@ -145,9 +104,10 @@ pub struct DoubleBuffer {
 
 impl DoubleBuffer {
     pub fn new(cfg: &Config) -> DoubleBuffer {
+        // Add a row of padding at the bottom
         DoubleBuffer {
-            buf_a: vec![0_u8; cfg.size],
-            buf_b: vec![0_u8; cfg.size],
+            buf_a: vec![0_u8; cfg.size + cfg.width],
+            buf_b: vec![0_u8; cfg.size + cfg.width],
             count: 0,
         }
     }
@@ -182,13 +142,22 @@ impl DoubleBuffer {
 }
 
 pub struct Grid {
-    cfg: Config,
+    cfg: Arc<Config>,
     buf: DoubleBuffer,
+}
+
+fn generate_target_ribbons(
+    target: &mut Vec<u8>,
+    width: usize,
+    ribbon_len: usize,
+) -> Vec<&mut [u8]> {
+    let (_, target_without_first_row) = target.split_at_mut(width);
+    target_without_first_row.chunks_mut(ribbon_len).collect()
 }
 
 impl Grid {
     pub fn new(width: usize, height: usize) -> Grid {
-        let cfg = Config::new(width, height);
+        let cfg = Arc::new(Config::new(width, height));
         let buf = DoubleBuffer::new(&cfg);
         Grid { cfg, buf }
     }
@@ -204,90 +173,69 @@ impl Grid {
     pub fn spawn(&mut self, frame: u32) {
         let source = self.buf.get_front_mut();
         if frame % 4 == 0 {
-            for _ in 0..(self.cfg.width / 20 + 1) {
-                let i = self.cfg.rng.gen_range(0..self.cfg.width);
-                if source[i] == 0 {
-                    source[i] = ((frame / 5) % 254 + 1) as u8;
+            for i in 0..(self.cfg.width / 4) {
+                // let i = self.cfg.rng.gen_range(0..self.cfg.width);
+                let j = i * 4;
+                if source[j] == 0 {
+                    source[j] = ((frame / 5) % 254 + 1) as u8;
                 }
             }
         }
     }
 
+    pub fn next(&mut self, frame: u32) {
+        self.spawn(frame);
+
+        let ribbon_len = self.cfg.ribbon_len;
+        let (source, target) = self.buf.get_pair();
+        target.fill(0);
+        let source_ribbons: Vec<&[u8]> =
+            source.chunks_exact(ribbon_len).collect();
+        let mut target_ribbons =
+            generate_target_ribbons(target, self.cfg.width, ribbon_len);
+
+        scope(|s| {
+            for (i, (source, target)) in source_ribbons
+                .iter()
+                .zip(target_ribbons.iter_mut())
+                .enumerate()
+            {
+                let cfg = Arc::clone(&self.cfg);
+                s.spawn(move |_| {
+                    for j in 0..ribbon_len {
+                        let real_index = i * ribbon_len + j;
+                        next_pixel(&cfg, source, target, j, real_index)
+                    }
+                });
+            }
+        })
+        .unwrap();
+
+        println!("Frame {frame}");
+        // thread::sleep(time::Duration::from_millis(200));
+
+        self.buf.switch_buffers();
+    }
+
     // pub fn next(&mut self, frame: u32) {
     //     self.spawn(frame);
     //     let (source, target) = self.buf.get_pair();
-
-    //     let firsts = self.cfg.split(
-    //         source,
-    //         0,
-    //         self.cfg.ribbon_len * 2,
-    //         self.cfg.ribbon_len,
-    //     );
-    //     let seconds = self.cfg.split_mut(
-    //         target,
-    //         0,
-    //         self.cfg.ribbon_len * 2,
-    //         self.cfg.ribbon_len + 1,
-    //     );
-
-    //     // let mut handles: Vec<thread::JoinHandle<()>> = Vec::new();
-
-    //     for i in 0..self.cfg.n_threads {
-    //         let start_i = 2 * i * self.cfg.ribbon_len;
-    //         let source_ribbon = &source[start_i..start_i + self.cfg.ribbon_len];
-    //         let target_ribbon =
-    //             if start_i + self.cfg.ribbon_len + self.cfg.width
-    //                 <= self.cfg.size
-    //             {
-    //                 &mut target[start_i
-    //                     ..start_i + self.cfg.ribbon_len + self.cfg.width]
-    //             } else {
-    //                 &mut target[start_i..start_i + self.cfg.ribbon_len]
-    //             };
-    //         let mut f = || {
-    //             next_pixel(
-    //                 &self.cfg,
-    //                 source_ribbon,
-    //                 target_ribbon,
-    //                 self.cfg.ribbon_len,
-    //                 i,
-    //             )
-    //         };
-    //         f ()
-    //         // handles.push(f);
-    //         // handles.push(thread::spawn(f));
+    //     target.fill(0);
+    //     for i in 0..self.cfg.size {
+    //         next_pixel(&mut self.cfg, source, target, i)
     //     }
-
-    //     // for mut h in handles {
-    //     //     h.join().unwrap();
-    //     // }
-
     //     self.buf.switch_buffers();
-
-    //     // for i in (0..self.cfg.size).rev() {
-    //     //     next_pixel(rng, source, target, i)
-    //     // }
     // }
-
-    pub fn next(&mut self, frame: u32) {
-        self.spawn(frame);
-        let (source, target) = self.buf.get_pair();
-        target.fill(0);
-        for i in 0..self.cfg.size {
-            next_pixel(&mut self.cfg, source, target, i)
-        }
-        self.buf.switch_buffers();
-    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn internal() {
-        let g = Grid::new(4, 8);
-        let v = g.cfg.split(g.buf.get_front(), 0, 8, 4);
-        assert_eq!(v.len(), 4);
-    }
-}
+//     #[test]
+//     fn internal() {
+//         let g = Grid::new(4, 8);
+//         let v = g.cfg.split(g.buf.get_front(), 0, 8, 4);
+//         assert_eq!(v.len(), 4);
+//     }
+// }
