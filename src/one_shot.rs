8b153c4f50631b3287b1b9cd8a68b8a9d77bf535
benmandrew <benmandrew@gmail.com>
2024-06-16 16:05:59 +0200

    Fix BMP bug

--- src/one_shot.rs
+++ src/one_shot.rs
@@ -1,10 +1,9 @@
 use crate::grid::Grid;
 use bmp_rust::bmp::BMP;
-use std::time::Instant;
 
 fn write_to_bmp(grid: &Grid, filename: &str) {
     let (w, h) = grid.get_dims();
-    let mut bmp = BMP::new(w as i32, h as u32, None);
+    let mut bmp = BMP::new(h as i32, w as u32, None);
     let dib_header = bmp.get_dib_header().unwrap();
     let header = bmp.get_header();
     let buf = grid.get_front();
@@ -30,23 +29,17 @@ fn write_to_bmp(grid: &Grid, filename: &str) {
 }
 
 pub fn main_terminal(grid: &mut Grid, n_iterations: usize) {
-    // let now = Instant::now();
     for i in 0..n_iterations {
         grid.spawn(i as u32);
         grid.next()
     }
-    // let elapsed_time = now.elapsed();
-    // println!("{} ms", elapsed_time.as_millis());
     println!("{}", grid)
 }
 
 pub fn main_bmp(grid: &mut Grid, n_iterations: usize, filename: &str) {
-    let now = Instant::now();
     for i in 0..n_iterations {
         grid.spawn(i as u32);
         grid.next()
     }
-    let elapsed_time = now.elapsed();
-    println!("{} ms", elapsed_time.as_millis());
     write_to_bmp(grid, filename)
 }
